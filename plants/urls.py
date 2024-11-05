c2d2654eb0f535474b4a4c3827c6a0336ef6b645
mhasnainiftikhar <sp21-bse-014@cuilahore.edu.pk>
2024-11-06 00:40:33 +0500

    Update views

--- plants/urls.py
+++ plants/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
     path('', views.index, name='index'),
     path('register/', views.register, name='register'),
     path('login/', views.login_view, name='login'),
-    path('plants/', views.manage_plants, name='manage_plants'),
+    path('plants/', views.list_plants, name='list_plants'),  # Changed to list_plants
+    path('plants/create/', views.create_plant, name='create_plant'),  # Added new URL for create_plant
 ]
