f8dd42858d1282c83f3831370220793deb24194e
mhasnainiftikhar <sp21-bse-014@cuilahore.edu.pk>
2024-11-05 20:43:19 +0500

    second

--- plantTech/settings.py
+++ plantTech/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-dl-+qc%b7rjnj)qs@v&lxv1_&k=b7(335ybbvvtzk90bl8zrk#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 # Application definition
 
