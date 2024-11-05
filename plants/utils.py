e28b3108c22f765ff33a2ae5831de7d8fddec3ea
mhasnainiftikhar <sp21-bse-014@cuilahore.edu.pk>
2024-11-05 22:04:03 +0500

    third

--- plants/utils.py
+++ plants/utils.py
@@ -1,3 +1,46 @@
+from firebase_admin import firestore
+
+def save_user_to_firestore(user):
+    db = firestore.client()
+    users_ref = db.collection('users')
+    users_ref.add({
+        'username': user.username,
+        'email': user.email,
+        'role': user.role,
+        'timestamp': firestore.SERVER_TIMESTAMP
+    })
+
+def save_plant_to_firestore(plant):
+    db = firestore.client()
+    plants_ref = db.collection('plants')
+    plants_ref.add({
+        'user_id': plant.user.id,
+        'name': plant.name,
+        'planting_date': plant.planting_date,
+        'soil_type': plant.soil_type,
+        'timestamp': firestore.SERVER_TIMESTAMP
+    })
+
+def save_growth_history_to_firestore(growth_history):
+    db = firestore.client()
+    growth_history_ref = db.collection('growth_history')
+    growth_history_ref.add({
+        'plant_id': growth_history.plant.id,
+        'date': growth_history.date,
+        'growth_stage': growth_history.growth_stage,
+        'timestamp': firestore.SERVER_TIMESTAMP
+    })
+
+def save_notification_to_firestore(user_id, title, body):
+    db = firestore.client()
+    notifications_ref = db.collection('notifications')
+    notifications_ref.add({
+        'user_id': user_id,
+        'title': title,
+        'body': body,
+        'timestamp': firestore.SERVER_TIMESTAMP
+    })
+
 from firebase_admin import messaging
 
 
@@ -11,3 +54,15 @@ def send_firebase_notification(user_token, title, body):
     )
     response = messaging.send(message)
     return response
+
+def get_notifications_from_firestore(user_id):
+    db = firestore.client()
+    notifications_ref = db.collection('notifications')
+    notifications = notifications_ref.where('user_id', '==', user_id).get()
+    return [{
+        'id': doc.id,
+        'data': doc.to_dict()
+    } for doc in notifications]
+
+
+
--- plants/views.py
+++ plants/views.py
@@ -5,7 +5,7 @@ from rest_framework.permissions import IsAuthenticated
 from .models import User, Plant, Notification
 from .serializers import UserSerializer, PlantSerializer, NotificationSerializer
 from django.contrib.auth import authenticate
-from .utils import send_firebase_notification
+from .utils import get_notifications_from_firestore, save_notification_to_firestore, save_notification_to_firestore, save_plant_to_firestore, save_user_to_firestore, send_firebase_notification
 
 from django.shortcuts import render
 
@@ -13,14 +13,17 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'index.html')
 
-
 @api_view(['POST'])
 def register(request):
     serializer = UserSerializer(data=request.data)
     if serializer.is_valid():
-        serializer.save()
+        user = serializer.save()
+        save_user_to_firestore(user)  # Save user to Firestore
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
 
 
 @api_view(['POST'])
@@ -39,7 +42,8 @@ def manage_plants(request):
     if request.method == 'POST':
         serializer = PlantSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save(user=request.user)
+            plant = serializer.save(user=request.user)
+            save_plant_to_firestore(plant)  # Save plant to Firestore
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     else:
@@ -57,5 +61,23 @@ def send_notification(request):
 
     if user_token and title and body:
         response = send_firebase_notification(user_token, title, body)
+        save_notification_to_firestore(request.user.id, title, body)  # Save to Firestore
         return Response({"message": "Notification sent", "firebase_response": response}, status=status.HTTP_200_OK)
     return Response({"error": "Missing data"}, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def send_notification(request):
+    user_token = request.data.get('user_token')
+    title = request.data.get('title')
+    body = request.data.get('body')
+
+    if user_token and title and body:
+        response = send_firebase_notification(user_token, title, body)
+        return Response({"message": "Notification sent", "firebase_response": response}, status=status.HTTP_200_OK)
+    return Response({"error": "Missing data"}, status=status.HTTP_400_BAD_REQUEST)
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def fetch_notifications(request):
+    notifications = get_notifications_from_firestore(request.user.id)
+    return Response(notifications, status=status.HTTP_200_OK)
